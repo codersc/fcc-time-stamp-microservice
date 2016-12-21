31cc127a614792ba73966ca7ae73cd86abdb8675
Kevin Colten Holmes <coltenh708@msn.com>
2016-12-21 12:08:56 +0000

    updated server.js and added index.html

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
+var months = {
+    '0': 'January',
+    '1': 'February',
+    '2': 'March',
+    '3': 'April',
+    '4': 'May',
+    '5': 'June',
+    '6': 'July',
+    '7': 'August',
+    '8': 'September',
+    '9': 'October',
+    '10': 'November',
+    '11': 'December',
+};
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/:date', function (req, res) {
+    var date = req.params.date
+    if (isNaN(parseInt(date)) && isNaN(new Date(date))) {
+        res.json({ "unix": null, "natural": null });
+    } else if (!isNaN(new Date(parseInt(date)))) {
+        date = new Date(parseInt(date));
+        res.json({
+            "unix": date.getTime(),
+            "natural": months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear()
+        });        
+    } else {
+        date = new Date(date);
+        res.json({
+            "unix": date.getTime(),
+            "natural": months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear()
+        });
+    }
+});
+
+app.listen(8080);
